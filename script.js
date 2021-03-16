abae071d7c7f738a9327857bb8b0ebc6cac2a7b1
Glitch (storm-sincere-teacher) <none>
2021-03-16 16:25:38 +0000

    🍰🏜 Checkpoint ./README.md:29806850/28 ./script.js:29806850/184

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // global constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
 const cluePauseTime = 333; //how long to pause in between clues
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 
@@ -10,13 +9,14 @@ var gamePlaying = false;
 var tonePlaying = false;
 var volume = 0.5; //must be between 0.0 and 1.0
 var guessCounter = 0;
+var clueHoldTime = 1000; 
 
 function startGame() {
   //initialize game variables
   progress = 0;
   gamePlaying = true;
   // swap the Start and Stop buttons
-
+  
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
   playClueSequence();
@@ -48,6 +48,7 @@ function playClueSequence() {
   guessCounter = 0;
   let delay = nextClueWaitTime; //set delay to initial wait time
   for (let i = 0; i <= progress; i++) {
+    clueHoldTime -= 100;
     // for each clue that is revealed so far
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms");
     setTimeout(playSingleClue, delay, pattern[i]); // set a timeout to play that clue
@@ -63,7 +64,7 @@ function loseGame() {
 
 function winGame() {
   stopGame();
-  alert("Game Over. You won!.");
+  alert("Game Over. You won!");
 }
 
 function guess(btn) {
